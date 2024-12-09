6df0e098cca88140d6d4e58e54bdb733a0d2e731
Jerry Bruwes <jbruwes@gmail.com>
2024-12-09 17:39:49 +0200

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,64 @@
-import type { TImportmap, TPage } from "@vues3/types";
+import type { FromSchema } from "json-schema-to-ts";
 import type { ComputedRef, Reactive } from "vue";
 
 import useFlatJsonTree from "@vues3/flat-json-tree";
-import { validateData } from "@vues3/types";
+import Ajv from "ajv";
+import dynamicDefaults from "ajv-keywords/dist/definitions/dynamicDefaults";
+import uuid from "uuid-random";
 import { reactive, watch } from "vue";
 
+import Credentials from "./schemas/credentials";
+import Data from "./schemas/data";
+import Importmap from "./schemas/importmap";
+import Page, { plainPage } from "./schemas/page";
+
+export type TPage = FromSchema<typeof plainPage> & {
+  $children?: TPage[];
+  $index: number;
+  $next?: TPage;
+  $prev?: TPage;
+  $siblings: TPage[];
+  branch: TPage[];
+  buffer?: string;
+  children?: TPage[];
+  contenteditable: boolean;
+  html: Promise<string> | string;
+  i: string;
+  index: number;
+  next?: TPage;
+  parent?: TPage;
+  path: string;
+  prev?: TPage;
+  root: TPage;
+  sfc: Promise<string>;
+  siblings: TPage[];
+  title: string;
+  to: string;
+};
+dynamicDefaults.DEFAULTS.uuid = (): (() => string) => () => uuid();
+export type TCredentials = FromSchema<typeof Credentials>;
+export type TImportmap = FromSchema<typeof Importmap>;
+const ajv = (() => {
+  const esm = true;
+  const code = { esm };
+  const coerceTypes = true;
+  const keywords = [dynamicDefaults()];
+  const removeAdditional = true;
+  const schemas = [Credentials, Page, Data, Importmap];
+  const useDefaults = true;
+  return new Ajv({
+    code,
+    coerceTypes,
+    keywords,
+    removeAdditional,
+    schemas,
+    useDefaults,
+  });
+})();
+export const validatePage = ajv.getSchema("urn:jsonschema:page");
+export const validateCredentials = ajv.getSchema("urn:jsonschema:credentials");
+const validateData = ajv.getSchema("urn:jsonschema:data");
+const validateImportmap = ajv.getSchema("urn:jsonschema:importmap");
 export const deep = true;
 export const importmap = reactive({} as TImportmap);
 export const data: Reactive<TPage[]> = reactive([]);
@@ -66,7 +120,10 @@ const title = {
   },
 };
 watch(data, (value) => {
-  validateData(value);
+  validateData?.(value) as boolean;
+});
+watch(importmap, (value) => {
+  validateImportmap?.(value) as boolean;
 });
 watch(pages, (value) => {
   value.forEach((element) => {
