7e84c4bcb4b954c3c194d0f2f5814ffd05a76e43
Jerry Bruwes <jbruwes@gmail.com>
2025-01-03 18:39:15 +0200

    modified:   src/importmap.ts

--- src/importmap.ts
+++ src/importmap.ts
@@ -1,19 +1,45 @@
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
+
 import type { JSONSchema } from "json-schema-to-ts";
+import type { JSONSchemaType } from "json-schema-to-ts/lib/types/definitions/jsonSchema";
+
+/* -------------------------------------------------------------------------- */
+/*                                  Constants                                 */
+/* -------------------------------------------------------------------------- */
 
 const $id = "urn:jsonschema:importmap";
-const additionalProperties = false;
-const properties = {
+
+/* -------------------------------------------------------------------------- */
+
+const additionalProperties: JSONSchema = false;
+
+/* -------------------------------------------------------------------------- */
+
+const type: JSONSchemaType = "object";
+
+/* -------------------------------------------------------------------------- */
+
+const properties: Readonly<Record<string, JSONSchema>> = {
   imports: {
     additionalProperties: { type: "string" },
     default: {},
-    type: "object",
+    type,
   },
 } as const;
-const type = "object";
 
-export default {
+/* -------------------------------------------------------------------------- */
+
+const importmap = {
   $id,
   additionalProperties,
   properties,
   type,
 } as const satisfies JSONSchema;
+
+/* -------------------------------------------------------------------------- */
+/*                                   Exports                                  */
+/* -------------------------------------------------------------------------- */
+
+export default importmap;
