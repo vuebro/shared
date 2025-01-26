3c80f2d3bc562ad2e6d8e17a1658d5a44468198f
Jerry Bruwes <jbruwes@gmail.com>
2025-01-26 19:02:26 +0200

    modified:   src/importmap.ts

--- src/importmap.ts
+++ src/importmap.ts
@@ -1,49 +1,23 @@
-/* -------------------------------------------------------------------------- */
-/*                                   Imports                                  */
-/* -------------------------------------------------------------------------- */
-
 import type { JSONSchema } from "json-schema-to-ts";
-import type { JSONSchemaType } from "json-schema-to-ts/lib/types/definitions/jsonSchema";
-
-/* -------------------------------------------------------------------------- */
-/*                                  Constants                                 */
-/* -------------------------------------------------------------------------- */
-
-const $id = "urn:jsonschema:importmap";
 
 /* -------------------------------------------------------------------------- */
 
-const additionalProperties: JSONSchema = false;
+const $id = "urn:jsonschema:importmap",
+  additionalProperties = false,
+  type = "object",
+  properties = {
+    imports: {
+      additionalProperties: { type: "string" },
+      default: {},
+      type: "object",
+    },
+  } as const;
 
 /* -------------------------------------------------------------------------- */
 
-const type: JSONSchemaType = "object";
-
-/* -------------------------------------------------------------------------- */
-/*                                   Objects                                  */
-/* -------------------------------------------------------------------------- */
-
-const properties = {
-  imports: {
-    additionalProperties: { type: "string" },
-    default: {},
-    type: "object",
-  },
-} as const;
-
-/* -------------------------------------------------------------------------- */
-
-const importmap = {
+export default {
   $id,
   additionalProperties,
   properties,
   type,
 } as const satisfies JSONSchema;
-
-/* -------------------------------------------------------------------------- */
-/*                                   Exports                                  */
-/* -------------------------------------------------------------------------- */
-
-export default importmap;
-
-/* -------------------------------------------------------------------------- */
