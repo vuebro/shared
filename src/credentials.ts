5ffda54c8ad1795168394f006a428a8c05244621
Jerry Bruwes <jbruwes@gmail.com>
2025-01-26 19:02:10 +0200

    modified:   src/credentials.ts

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,51 +1,26 @@
-/* -------------------------------------------------------------------------- */
-/*                                   Imports                                  */
-/* -------------------------------------------------------------------------- */
-
 import type { JSONSchema } from "json-schema-to-ts";
-import type { JSONSchemaType } from "json-schema-to-ts/lib/types/definitions/jsonSchema";
-
-/* -------------------------------------------------------------------------- */
-/*                                  Constants                                 */
-/* -------------------------------------------------------------------------- */
-
-const $id = "urn:jsonschema:credentials";
 
 /* -------------------------------------------------------------------------- */
 
 const nullable = true;
 
-/* -------------------------------------------------------------------------- */
-
-const type: JSONSchemaType = "object";
-
-/* -------------------------------------------------------------------------- */
-/*                                   Objects                                  */
-/* -------------------------------------------------------------------------- */
-
-const additionalProperties = {
-  properties: {
-    accessKeyId: { default: null, nullable, type: "string" },
-    Bucket: { default: null, nullable, type: "string" },
-    endpoint: { default: null, nullable, type: "string" },
-    region: { default: null, nullable, type: "string" },
-    secretAccessKey: { default: null, nullable, type: "string" },
-  },
-  type,
-} as const;
+const $id = "urn:jsonschema:credentials",
+  type = "object",
+  additionalProperties = {
+    properties: {
+      accessKeyId: { default: null, nullable, type: "string" },
+      Bucket: { default: null, nullable, type: "string" },
+      endpoint: { default: null, nullable, type: "string" },
+      region: { default: null, nullable, type: "string" },
+      secretAccessKey: { default: null, nullable, type: "string" },
+    },
+    type,
+  } as const;
 
 /* -------------------------------------------------------------------------- */
 
-const credentials = {
+export default {
   $id,
   additionalProperties,
   type,
 } as const satisfies JSONSchema;
-
-/* -------------------------------------------------------------------------- */
-/*                                   Exports                                  */
-/* -------------------------------------------------------------------------- */
-
-export default credentials;
-
-/* -------------------------------------------------------------------------- */
