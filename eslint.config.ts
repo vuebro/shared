7f20349005ac29594a7a072cbd7c21c398d1a3a7
Jerry Bruwes <jbruwes@gmail.com>
2025-01-26 19:01:17 +0200

    new file:   eslint.config.ts

---
+++ eslint.config.ts
@@ -0,0 +1,48 @@
+import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
+import type { Linter } from "eslint";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import eslintPluginImportX from "eslint-plugin-import-x";
+import perfectionist from "eslint-plugin-perfectionist";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint, { configs } from "typescript-eslint";
+
+import eslintrc from "./.eslintrc.json";
+
+/* -------------------------------------------------------------------------- */
+
+const projectService = true,
+  tsconfigRootDir = import.meta.dirname;
+
+/* -------------------------------------------------------------------------- */
+
+const parserOptions = { projectService, tsconfigRootDir };
+
+/* -------------------------------------------------------------------------- */
+
+const compat = new FlatCompat(),
+  ignores = ["index.js", "index.d.ts"],
+  languageOptions = { parserOptions },
+  rules: FlatConfig.Rules = {
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-use-before-define": "error",
+    "no-shadow": "off",
+    "no-use-before-define": "off",
+  };
+
+/* -------------------------------------------------------------------------- */
+
+export default tseslint.config(
+  { ignores },
+  { rules },
+  { languageOptions },
+  ...compat.config(eslintrc as Linter.LegacyConfig),
+  eslint.configs.recommended,
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
+  configs.strictTypeChecked,
+  configs.stylisticTypeChecked,
+  perfectionist.configs["recommended-natural"],
+  eslintPluginPrettierRecommended,
+);
