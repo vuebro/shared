949a668bf0a4c7b2c4025157ab3a9d9f1bb6e52a
Jerry Bruwes <jbruwes@gmail.com>
2025-01-29 14:09:40 +0200

    modified:   index.ts

--- index.ts
+++ index.ts
@@ -16,6 +16,7 @@ import Page from "./src/page";
 
 interface IFlatJsonTree {
   add: (pId: string) => string | undefined;
+  atlas: ComputedRef<Map<string, TPage>>;
   down: (pId: string) => void;
   leaves: ComputedRef<TPage[]>;
   left: (pId: string) => string | undefined;
