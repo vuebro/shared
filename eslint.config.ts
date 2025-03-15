dd525d990a73a3a960dea9ab8f122e1896937714
Jerry Bruwes <jbruwes@gmail.com>
2025-03-15 15:13:19 +0200

    modified:   eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,41 +1,35 @@
-import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-
 import eslint from "@eslint/js";
 import eslintPluginImportX from "eslint-plugin-import-x";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import { configs as sonarjs } from "eslint-plugin-sonarjs";
 import tseslint, { configs } from "typescript-eslint";
-
-const allowDefaultProject = ["eslint.config.ts"],
-  ignores = ["**/dist"],
-  projectService = { allowDefaultProject },
-  tsconfigRootDir = import.meta.dirname,
-  parserOptions = { projectService, tsconfigRootDir },
-  languageOptions = { parserOptions },
-  rules: FlatConfig.Rules = {
-    "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
-    "import-x/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: ["**/eslint.config.ts"],
-        optionalDependencies: false,
-      },
-    ],
-  };
-
-/** @internal */
 export default tseslint.config(
-  { ignores },
-  { rules },
-  { languageOptions },
+  { ignores: ["**/dist"] },
+  {
+    rules: {
+      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+      "import-x/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/eslint.config.ts"],
+          optionalDependencies: false,
+        },
+      ],
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: { allowDefaultProject: ["eslint.config.ts"] },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   eslint.configs.recommended,
   eslintPluginImportX.flatConfigs.recommended,
   eslintPluginImportX.flatConfigs.typescript,
   configs.strictTypeChecked,
   configs.stylisticTypeChecked,
-
-  sonarjs.recommended,
   perfectionist.configs["recommended-natural"],
   eslintPluginPrettierRecommended,
 );
