cac4055dba19ceb439dc52a7f2893d2b42c0ddd2
Jerry Bruwes <jbruwes@icloud.com>
2025-07-01 18:41:08 +0200

    modified:   src/types/data.ts

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,7 +1,6 @@
 import type { JSONSchema } from "json-schema-to-ts";
 export default {
   $id: "urn:jsonschema:data",
-  $schema: "https://json-schema.org/draft/2020-12/schema",
   items: { $ref: "urn:jsonschema:page" },
   type: "array",
 } as const satisfies JSONSchema;
