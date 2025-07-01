8903bcb445bf8e3f14c033a8e2746d4522f93e07
Jerry Bruwes <jbruwes@icloud.com>
2025-07-01 18:41:37 +0200

    modified:   src/types/page.ts

--- src/types/page.ts
+++ src/types/page.ts
@@ -1,6 +1,5 @@
 export default {
   $id: "urn:jsonschema:page",
-  $schema: "https://json-schema.org/draft/2020-12/schema",
   additionalProperties: false,
   dynamicDefaults: { id: "uuid" },
   properties: {
