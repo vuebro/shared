4d599f025ba592f4f1bf0f5cc5c95ef3d0d5d1bb
Jerry Bruwes <jbruwes@icloud.com>
2025-08-06 19:58:23 +0200

    new file:   src/types/fonts.ts

---
+++ src/types/fonts.ts
@@ -0,0 +1,6 @@
+import type { JSONSchema } from "json-schema-to-ts";
+export default {
+  $id: "urn:jsonschema:fonts",
+  items: { type: "string" },
+  type: "array",
+} as const satisfies JSONSchema;
