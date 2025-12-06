901338569cb18291b1b1e42574c005b02d44aabe
Jerry Bruwes <jbruwes@icloud.com>
2025-12-06 20:32:47 +0200

    Update page schema: replace frontmatter with head property

--- src/schemas/page.ts
+++ src/schemas/page.ts
@@ -31,12 +31,7 @@ export default {
     description: { default: null, nullable: true, type: "string" },
     enabled: { default: true, type: "boolean" },
     flat: { default: true, type: "boolean" },
-    frontmatter: {
-      default: null,
-      nullable: true,
-      properties: { head: { type: "object" } },
-      type: "object",
-    },
+    head: { default: {}, type: "object" },
     header: { type: "string" },
     icon: { type: "string" },
     id: { type: "string" },
